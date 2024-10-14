48150435b3144177ba23917ccc5b0a665ad30722
Wouter <woutpa@arteveldehs.be>
2024-10-14 16:15:42 +0200

    added some ui

--- src/app.py
+++ src/app.py
@@ -5,6 +5,9 @@ from image_processing import Camera
 def define_page():
     pass
 
+def click_button():
+    st.session_state.is_capturing = not st.session_state.is_capturing
+
 
 def main(camera: Camera):
 
@@ -15,18 +18,22 @@ def main(camera: Camera):
     st.caption("Powered by OpenCV, Streamlit")
     frame_placeholder = st.empty()
 
+    
+
+
     if 'last_frame' in st.session_state and st.session_state.last_frame is not None:
 
         print('processing.....')
+        st.title("Last Frame")
+
         print(st.session_state.last_frame)
-        frame_placeholder.image(st.session_state.last_frame, channels="RGB")
+        frame_placeholder_last_frame = st.empty()
+        frame_placeholder_last_frame.image(st.session_state.last_frame, channels="RGB")
         st.session_state.last_frame = None
 
-        st.rerun()
+        #st.rerun()
 
    
-    
-    print('current_index: ',  camera.current_camera_index)
 
     if len(camera.available_cameras) == 0:
         st.error("No cameras detected.")
@@ -37,8 +44,7 @@ def main(camera: Camera):
 
 
     button_text = "Stop" if st.session_state.is_capturing else "Start"
-
-    st.session_state.is_capturing = not st.button(button_text)
+    st.button(button_text, on_click=click_button)
 
     camera.take_image(st.session_state.is_capturing, frame_placeholder)
 
